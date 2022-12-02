b069bc059c33a6ab6e1d3cfab26c1b459ba07cee
Guillermo Fernandez <bengallme@gmail.com>
2022-12-01 20:39:48 -0600

    Added solutions for day 1 using golang

---
+++ go/day-1/part2.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+)
+
+func main() {
+
+	f, _ := os.Open(filepath.Base("input.txt"))
+
+	fileScanner := bufio.NewScanner(f)
+
+	calories := []int{}
+	var currentSum int
+
+	for fileScanner.Scan() {
+
+		line := fileScanner.Text()
+
+		if len(line) == 0 {
+			calories = append(calories, currentSum)
+			currentSum = 0
+			continue
+		}
+
+		lineCalories, _ := strconv.Atoi(line)
+
+		currentSum += lineCalories
+	}
+
+	calories = append(calories, currentSum)
+
+	sort.Ints(calories)
+
+	fmt.Println(calories[len(calories)-3] + calories[len(calories)-2] + calories[len(calories)-1])
+
+}
